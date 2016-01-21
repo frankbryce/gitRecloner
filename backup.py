a5bdc82c7eccfb2aec54105ca56f5b78ffa35f70
jonnyjack <jonnyjack7@gmail.com>
2016-01-20 22:49:44 -0500

    Restore created, but needs to be tested in a separate directory.

--- backup.py
+++ backup.py
@@ -8,15 +8,15 @@ def backup(outFile):
     lsOut = execCmd(["ls", "..", "-a"])
     gitDirs = lsOut.split("\n")
     for gitDir in gitDirs:
-      print(gitDir)
       relDir = "../"+gitDir
       if (gitDir==".") | (gitDir=="..") | (not os.path.isdir(relDir)):
         continue
+      print(gitDir)
       gitUrl = execCmd(["git", "--git-dir="+relDir+"/.git", "--work-tree="+relDir,
                         "config", "--get", "remote.origin.url"])
       if not gitUrl:
         continue
-    
+      
       out.write(gitUrl.strip())
       out.write("\n")
   finally:
--- restore.py
+++ restore.py
@@ -1,5 +1,9 @@
 from subprocess import call
 
-def restore():
-  gitRepos = call(["ls", "-a"])
-  print(gitRepos)
+def restore(inFile):
+  infile = open(inFile, "r")
+  try:
+    for repoUrl in [url.rstrip('\n') for url in infile]:
+      execCmd(["git", "--work-tree="+relDir,"clone", repoUrl])
+  finally:
+    infile.close()
