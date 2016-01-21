63bcd25daa62beefd6af644a87ca85e699de61fb
jonnyjack <jonnyjack7@gmail.com>
2016-01-20 22:52:20 -0500

    fixing restore bug

--- restore.py
+++ restore.py
@@ -1,9 +1,10 @@
 from subprocess import call
+from command import execCmd
 
 def restore(inFile):
   infile = open(inFile, "r")
   try:
     for repoUrl in [url.rstrip('\n') for url in infile]:
-      execCmd(["git", "--work-tree="+relDir,"clone", repoUrl])
+      execCmd(["git", "--work-tree=..","clone", repoUrl])
   finally:
     infile.close()
